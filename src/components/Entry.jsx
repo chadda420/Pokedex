9148c38e5bed214f14d13f5d42255c96b1f1981a
Chad <chaddawick@gmail.com>
2025-05-12 12:01:38 -0500

    Added search bar and button functionality

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -6,9 +6,38 @@ import axios from 'axios'
 export default function Entry(){
     const [pokemon, setPokemon] = useState(null)
     const [currentId, setCurrentId] = useState(1)
+    const [search, setSearch] = useState('')
+    const [error, setError] = useState('')
 
     const handleNext = () => {
         setCurrentId(prevId => prevId + 1)
+
+    }
+
+    const handlePrevious = () => {
+      if(currentId === 1){
+        return;
+      }
+        setCurrentId(prevId => prevId - 1)
+
+    }
+
+    const handleSearch = async () => {
+      if(!search.trim()) return;
+      try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search.toLowerCase()}`);
+        if(!response.ok) throw new Error('Pokemon not found');
+        const data = await response.json()
+        setPokemon(data)
+        setCurrentId(data.id)
+        setError('');
+      } catch(err){
+        setError(err.message)
+        setPokemon(null)
+      }
+    
+
+
 
     }
 
@@ -23,6 +52,8 @@ export default function Entry(){
             console.error('Error fetching Pokémon data:', error);
           });
       }, [currentId]);
+
+
    
 
 
@@ -30,6 +61,17 @@ export default function Entry(){
 
 return (
   <div className='pokedex-container'>
+    <input 
+    className='search-bar'
+    type="text"
+    value={search}
+    onChange={(e) => { setSearch(e.target.value)}}
+    placeholder='Search Pokemon' />
+    <button className="search-button" onClick={handleSearch}>Search</button>
+    {error && <p className="error-message">{error}</p>}
+
+    
+
     {pokemon ? (
       <>
         <h3>#: {currentId}</h3>
@@ -44,7 +86,10 @@ return (
     ) : (
       <p>Loading...</p>
     )}
+    <div className="button-row">
+    <button className="previous-button" onClick={handlePrevious}>Previous</button>
     <button className='next-button' onClick={handleNext}>Next</button>
+    </div>
   </div>
 );
 
